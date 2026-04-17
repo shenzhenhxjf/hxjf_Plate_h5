4c84b399a99c279e5815b5c102f55b5b9b514842
laptop <2183818092@qq.com>
2026-04-17 19:57:58 +0800

    fix: 基本结构

--- src/views/quote/index.vue
+++ src/views/quote/index.vue
@@ -68,69 +68,8 @@ const productsConfig = reactive({
   <ScreenScale>
     <div class="quote-container">
       <div class="content">
-        <div class="left_container">
-          <div class="left_content">
-            <div class="left_content_top">
-              <div class="logo_image">
-                <img :src="logoImage" alt="海峡金" class="image"/>
-              </div>
+        <div>
 
-
-              <!--              <div class="gold_price">-->
-              <!--                <div class="gold_price_title">黄金销售价</div>-->
-              <!--                <div class="gold_price_value">-->
-              <!--                  <span class="price_value">{{ formatPrice(productsConfig.data[0].price) }}</span>-->
-              <!--                  <span class="price_value_unit">元/克</span>-->
-              <!--                </div>-->
-              <!--              </div>-->
-
-              <div class="gold_price">
-                <div class="one">
-                  <div class="gold_price_title">黄金销售价</div>
-                  <div class="gold_price_value">
-                    <span class="price_value">{{ formatPrice(productsConfig.data[0].price) }}</span>
-                    <span class="price_value_unit">元/克</span>
-                  </div>
-                </div>
-                <div class="gold_image">
-                  <img :src="goldImage" alt="黄金" class="one_image"/>
-                </div>
-
-                <div class="one">
-                  <div class="gold_price_title">黄金代入库价</div>
-                  <div class="gold_take_up_value">
-                    <span class="price_value_one">{{ formatPrice(productsConfig.data[0].price as any - 4 )}}</span>
-                    <span class="price_value_unit">元/克</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="left_content_bottom">
-              <div class="bottom_table">
-                <div class="table_title">
-                  <span class="main_title">上海黄金交易所 (SGE)</span>
-                  <span class="minor_title">数据实时更新</span>
-                </div>
-                <div class="table_content">
-                  <div class="table_header">
-                    <div v-for="col in productsConfig.columns" :key="col.key" class="table-header-content" >
-                      {{ col.label}}
-                    </div>
-                  </div>
-                  <!--                  <div class="table_divider"></div>-->
-                  <div class="table_body">
-                    <div v-for=" (item, index) in productsConfig.data" :key="item.category" v-show="index !== 0" class="table-row">
-                      <div class="table-cell">{{ item.category }}</div>
-                      <div class="table-cell price-cell">{{ formatPrice(item.price) }}</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="right_container">
-          <img :src="rightImage" alt="海峡金" class="image_content"/>
         </div>
       </div>
     </div>
@@ -139,241 +78,27 @@ const productsConfig = reactive({
 
 <style scoped lang="scss">
 .quote-container {
-  width: 1920px;
-  height: 1080px;
-  background-image: url("@/assets/bg_logo/hjxbg.png");
-  background-size: cover;
-  background-position: center center;
-  background-repeat: no-repeat;
+  position: relative;
+  width: 1080px;
+  height: 1920px;
+  overflow: hidden;
+  background: #F5F5F5;
+}
+
+.quote-container::before {
+  content: '';
+  position: absolute;
+  inset: 0;
+  background: url("@/assets/new_h5_bg/new_bg.png") center center / cover no-repeat;
+  z-index: 0;
+
 }
 
 .content {
+  position: relative;
+  z-index: 1;
   width: 100%;
   height: 100%;
-  display: flex;
-  padding: 142px 30px 0 30px;
-  gap: 10px;
-  box-sizing: border-box;
 }
-.left_container {
-  width: 1225px;
-  height: calc(100% - 38px);
-  background-color: #fff;
-  border-radius: 8px;
-}
-.right_container {
-  width: 620px;
-  height: calc(100% - 38px);
-  //background-color: blue;
-}
-.image_content {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.left_content_top {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
-.logo_image {
-  margin: 40px 0 30px 0;
-}
-
-.logo_image .image {
-  width: 280px;
-  height: 125px
-}
-//.gold_price {
-//  width: 1080px;
-//  height: 172px;
-//  background: #F8F8F8;
-//  border-radius: 12px 12px 12px 12px;
-//  border: 1px solid #DDDDDD;
-//  display: flex;
-//  flex-direction: column;
-//  align-items: center;
-//  justify-content: center;
-//  gap: 20px;
-//}
-//.gold_price_title {
-//  width: 120px;
-//  height: 24px;
-//  font-family: PingFang SC, PingFang SC;
-//  font-weight: 500;
-//  font-size: 24px;
-//  color: #333333;
-//  line-height: 24px;
-//  text-align: center;
-//  font-style: normal;
-//  text-transform: none;
-//}
-.gold_price_value {
-
-}
-//.price_value {
-//  font-family: PingFang SC, PingFang SC;
-//  font-weight: 600;
-//  font-size: 48px;
-//  color: #F92424;
-//  line-height: 48px;
-//  text-align: center;
-//  font-style: normal;
-//  text-transform: none;
-//}
-
-
-/*添加代入库*/
-.gold_price {
-  width: 1080px;
-  height: 172px;
-  background: #F8F8F8;
-  border-radius: 12px;
-  border: 1px solid #DDDDDD;
-  display: flex;
-  //flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 100px;
-}
-.gold_image .one_image {
-  width: 80px;
-  height: auto;
-}
-.gold_price_title {
-  width: 145px;
-  height: 24px;
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 500;
-  font-size: 24px;
-  color: #333333;
-  line-height: 24px;
-  text-align: center;
-  font-style: normal;
-  text-transform: none;
-}
-.one {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 12px;
-}
-.price_value {
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 600;
-  font-size: 48px;
-  color: #F92424;
-  line-height: 48px;
-  text-align: center;
-  font-style: normal;
-  text-transform: none;
-}
-.price_value_one {
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 600;
-  font-size: 48px;
-  color: #006400;
-  line-height: 48px;
-  text-align: center;
-  font-style: normal;
-  text-transform: none;
-}
-/*添加代入库*/
-
-.left_content_bottom {
-  margin-top: 50px;
-}
-.bottom_table {
-  display: flex;
-  flex-direction: column;
-  padding: 0 60px;
-}
-.table_title {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.table_title .main_title {
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 500;
-  font-size: 24px;
-  color: #333333;
-  line-height: 24px;
-  text-align: left;
-  font-style: normal;
-  text-transform: none;
-}
-.table_title .minor_title {
-  //width: 152px;
-  //height: 36px;
-  background-color: #EAF4FF;
-  padding: 6px 16px;
-  margin-left: 20px;
-  border-radius: 8px;
-
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 400;
-  font-size: 20px;
-  color: #20518A;
-  line-height: 20px;
-  text-align: left;
-  font-style: normal;
-  text-transform: none;
-}
-.table_content {
-  //margin-top:30px;
-  padding: 0 30px;
-}
-.table_header {
-  display: flex;
-  justify-content: space-between;
-
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 500;
-  font-size: 20px;
-  color: #666666;
-  line-height: 20px;
-  text-align: left;
-  font-style: normal;
-  text-transform: none;
-
-  border-bottom: 2px solid #E5E5E5;
-
-
-}
-.table-header-content {
-  padding: 27px;
-}
-.table_body {
-
-}
-.table-row {
-  display: flex;
-  justify-content: space-between;
-  padding: 24px;
-  border-bottom: 2px solid #E5E5E5;
-}
-.table-cell {
-  font-family: PingFang SC, PingFang SC;
-  font-weight: 500;
-  font-size: 35px;
-  color: #1A1A1A;
-  line-height: 24px;
-  text-align: left;
-  font-style: normal;
-  text-transform: none;
-}
-.price-cell {
-  color: #F92424;
-}
-
-
-
-
-
-
 
 </style>
